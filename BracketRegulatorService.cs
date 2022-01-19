21df9832edb5242fd1e88b901238d24e01496ebb
Navneet Kaul <62413630+nkaul2033@users.noreply.github.com>
2022-01-18 17:18:03 -0700

    Add project files.

---
+++ BracketRegulatorService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobNimbusAssigmnment
+{
+    public class BracketRegulatorService
+    {
+        public BracketRegulatorService() { }
+
+        public Boolean isMatching(char char1,
+                              char char2)
+        {
+            if (char1 == '(' && char2 == ')')
+                return true;
+            else if (char1 == '{' && char2 == '}')
+                return true;
+            else if (char1 == '[' && char2 == ']')
+                return true;
+            else
+                return false;
+        }
+
+
+        public Boolean areBracketsEqual(char[] key)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < key.Length; i++)
+            {
+
+                if (key[i] == '{' || key[i] == '('
+                    || key[i] == '[')
+                    stack.Push(key[i]);
+
+
+                if (key[i] == '}' || key[i] == ')'
+                    || key[i] == ']')
+                {
+
+
+                    if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    else if (!isMatching(stack.Pop(),
+                                             key[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if (stack.Count == 0)
+                return true;
+            else
+            {
+
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JobNimbusAssigmnment
+{
+    class Program
+    {
+        static BracketRegulatorService bracketRegulatorService = new BracketRegulatorService();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello. Please Enter a string with matching brackets!");
+
+            while (true)
+            {
+                string key = Console.ReadLine();
+
+                if (bracketRegulatorService.areBracketsEqual(key.ToCharArray()))
+                    Console.WriteLine("TRUE");
+                else
+                    Console.WriteLine("FALSE");
+
+                continue;
+            }
+        }
+
+
+
+    }
+}
